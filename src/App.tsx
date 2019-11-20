f10f2155a2dcfac75832cfdf150e1f45e153efe3
🦑Svarba, Rastislav <rastislav.svarba@merck.com>
2019-11-20 11:35:06 +0100

    ✨ Add the modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Modal} from './Modal'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Modal message="something here"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
