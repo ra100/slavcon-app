2aa9759e4dd56646e12b42b3146ec22f178ccd64
🦑Svarba, Rastislav <rastislav.svarba@merck.com>
2019-12-11 11:38:07 +0100

    ✨ Add cats

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {Button} from './Button'
+type Data = {file: string}
 type Props = {message: string; otherMessage?: string}
-type State = {hidden: boolean}
+type State = {
+  hidden: boolean
+  catImage?: string
+}
 
-// @TODO next fetch some data
+// https://slavcon.sk/export/2019/program.json
+// https://aws.random.cat/meow
 
 export class Modal extends React.Component<Props, State> {
-  state = {hidden: false}
+  state = {hidden: false, catImage: undefined}
   static defaultProps = {otherMessage: 'I should be worthy'}
 
   public render() {
@@ -14,15 +19,37 @@ export class Modal extends React.Component<Props, State> {
       <div>
         {this.state.hidden ? this.props.otherMessage : this.props.message}
         <Button onClick={this.handleButtonClick}>
-          {this.state.hidden ? 'Make me worthy' : 'Welcome'}
+          {this.state.hidden ? 'Show cat' : 'Hide cat'}
         </Button>
+        {this.state.catImage && (
+          <img src={this.state.catImage} alt="I am a kitty cat" />
+        )}
       </div>
     )
+  }
+
+  private fetchCat = async () => {
+    try {
+      const response = await fetch('https://aws.random.cat/meow')
+      const data: Data = await response.json()
+      this.setState({catImage: data.file})
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   private handleButtonClick = () => {
     console.log('Tooogle')
     const currentState = this.state.hidden
-    this.setState({hidden: !currentState})
+    const nextState: State = {hidden: !currentState}
+    // @TODO ASK NEXT how to setState Partial??
+    // @TODO spinner, error handling
+    // @TODO lifecycle load
+    if (currentState) {
+      this.fetchCat()
+    } else {
+      nextState.catImage = undefined
+    }
+    this.setState(nextState)
   }
 }
