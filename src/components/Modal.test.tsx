06b96e25ac950cf1258d604359969b32644de18c
🦑Svarba, Rastislav <rastislav.svarba@merck.com>
2020-01-08 12:38:06 +0100

    ✅ Add simple modal test

---
+++ src/components/Modal.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-dom/test-utils'
+import {Modal} from './Modal'
+
+describe('Modal', () => {
+  it('should render', () => {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+    ReactDOM.render(<Modal message="Hello, jester!" />, container)
+    const button = container.querySelector('[test-id="modal"] button')
+    expect(button && button.textContent).toBe('Hide cat')
+  })
+  it('let us click', () => {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+    ReactDOM.render(<Modal message="Hello, jester!" />, container)
+    const button = container.querySelector('[test-id="modal"] button')
+    if (!button) {
+      throw new Error('uff.. button is missing')
+    }
+    TestUtils.Simulate.click(button)
+    expect(button.textContent).toBe('Show cat')
+  })
+
+  // TODO: start with meaningful stuff here
+  // TODO: load data from json SlavCon
+})
